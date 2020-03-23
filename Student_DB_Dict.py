7cea6a28bbd621ba862c03bea31df20c58a4ab72
amol jewalikar <62540556+amoljewalikar@users.noreply.github.com>
2020-03-23 15:04:12 +0530

    Add files via upload

---
+++ Student_DB_Dict.py
@@ -0,0 +1,61 @@
+std={}
+while True:
+    print("\nTo Insert student's detail press '1' :\nTo Delete student's detail press '2' :\n\
+To Search student's detail press '3' :\nTo see all students record press '4' :\nTo calculate Percentage of student press '5' :\n\
+To exit press '6' :")#To display Dict press 5:
+    i=int(input())
+    if i==1:
+        n=int(input("Enter how many student details you want to insert : "))
+        for i in range(n):
+            roll=int(input("Enter a Roll Number : "))
+            std[roll]={}
+            std[roll]['Name']=input("Enter a name of Student : ")
+            std[roll]['Class']=input("Enter student's class std : ")
+            std[roll]['Marks']={}
+            std[roll]['Marks']['Maths']=input("Enter marks of Maths subject : ")
+            std[roll]['Marks']['Science']=input("Enter marks of Science subject : ")
+            std[roll]['Marks']['English']=input("Enter marks of English subject : ")
+            std[roll]['Marks']['Programming']=input("Enter marks of Programming subject : ")
+            std[roll]['Marks']['Electronics']=input("Enter marks of Electronics subject : ")
+    elif i==2:
+        roll=int(input("Enter a Roll Number to delete student record : "))
+        if roll in std:
+            del(std[roll])
+            print("\nStudent's Record Deleted Successfully.")
+        else:
+            print("\nEntered Roll number Does not Exists!")
+    elif i==3:
+        roll=int(input("Enter a Roll Number to find Student: "))
+        if roll not in std:
+            print("\nEntered Roll number Does not Exists!")
+        else:
+            print("\n Details of entered Roll number :\n",std[roll])
+    elif i==4:
+        print("\n Students Database:\n",std)
+    elif i==5:
+        roll=int(input("Enter a Roll Number to check Percentage and Grade of student : "))
+        if roll not in std:
+            print("\nEntered Roll number Does not Exists!")
+        else:
+            per=(int(std[roll]['Marks']['Maths'])+int(std[roll]['Marks']['Science'])+int(std[roll]['Marks']['English'])+\
+                 int(std[roll]['Marks']['Programming'])+int(std[roll]['Marks']['Electronics'])) / 5
+            if int(std[roll]['Marks']['Maths']) < 40 or int(std[roll]['Marks']['Science']) < 40 or int(std[roll]['Marks']['English']) < 40\
+               or int(std[roll]['Marks']['Programming']) < 40 or int(std[roll]['Marks']['Electronics']) < 40:
+                print("\nName:",std[roll]['Name'],"Percentage : ",per,"Grade : F ")
+            else:
+                if per>=75:
+                    print("\nName:",std[roll]['Name'],"Percentage : ",per,"Grade : O ")
+                elif per>=65 and per<75:
+                    print("\nName:",std[roll]['Name'],"Percentage : ",per,"Grade : A ")
+                elif per>=55 and per<65:
+                    print("\nName:",std[roll]['Name'],"Percentage : ",per,"Grade : B ")
+                elif per>=50 and per<55:
+                    print("\nName:",std[roll]['Name'],"Percentage : ",per,"Grade : C ")
+                elif per>=45 and per<50:
+                    print("\nName:",std[roll]['Name'],"Percentage : ",per,"Grade : D ")
+                elif per>=40 and per<45:
+                    print("\nName:",std[roll]['Name'],"Percentage : ",per,"Grade : E ") 
+    elif i==6:
+        break
+    
+          
