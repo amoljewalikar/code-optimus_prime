d69049b5f198c77e6aff73e24c4cc190caf89b1b
amol jewalikar <62540556+amoljewalikar@users.noreply.github.com>
2022-01-27 17:20:33 +0530

    Update prnt_str_ls.py

--- prnt_str_ls.py
+++ prnt_str_ls.py
@@ -1,12 +1,13 @@
 str1="My name is Amol"
 lstr=str1.split() #take into list
+print(lstr)
 j=[]
 n=[]
 for i in lstr:
     if len(i)==4:
         j.append(i)
 for k in j:
-    for m in range (0,len(j)+1):
+    for m in range (1,len(k)+1):
         #print(k[:m+1],end=' ')
-        n.append(k[:m+1])
+        n.append(k[:m])
 print(n)
